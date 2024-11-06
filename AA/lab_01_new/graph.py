2f1cb9ad30c30df5a755a1053153c7445f325dff
rollingbolling <dan.kazaryan@gmail.com>
2024-11-06 12:44:08 +0300

    lab_01

--- AA/lab_01_new/graph.py
+++ AA/lab_01_new/graph.py
@@ -1,38 +1,38 @@
-import matplotlib.pRecplot as plt
+import matplotlib.pyplot as plt
 import csv
 
+def create_graph(algo_1, algo_2, algo_3, algo_4, seconds, length):
+    fig, ax = plt.subplots()
+    if algo_1 != None:
+        ax.plot(seconds, algo_1[:length], label="Левенштейн итеративный", marker='', ls='-.')
+    if algo_2 != None:
+        ax.plot(seconds, algo_2[:length], label="Левенштейн рекурсивный", marker='', ls='--')
+    if algo_3 != None:
+        ax.plot(seconds, algo_3[:length], label="Левенштейн рекурсивный с кэшем", marker='', ls=':')
+    if algo_4 != None:
+        ax.plot(seconds, algo_4[:length], label="Дамерау-Левенштейн рекурсивный", marker='', ls='--')
+
+    ax.legend()
+    ax.grid()
+
+    plt.show()
+
+Sec1 = [float(i) for i in range(0, 13)]
+Sec2 = [float(i) % 1000 for i in range(1000-1)]
 NoRec = []
 Rec = []
-# RecCash = []
-# DamRec = []
-
+RecCash = []
+DamRec = []
 
 with open('measure.csv', 'r') as datafile:
     plotting = csv.reader(datafile, delimiter=' ')
     
     for ROWS in plotting:
         NoRec.append(float(ROWS[0]))
-        #NoRec.append('{} {}'.format(ROWS[3], ROWS[4]))
         Rec.append(float(ROWS[1]))
-        # RecCash.append(float(ROWS[2]))
-        # DamRec.append(float(ROWS[3]))
+        RecCash.append(float(ROWS[2]))
+        DamRec.append(float(ROWS[3]))
 
-print ('NoRec {} Nums'.format(len(NoRec)))
-print ('Rec {} Nums'.format(len(Rec)))
+create_graph(NoRec, Rec, RecCash, DamRec, Sec1, 13)
 
-# threshold=63.0
-# T=[threshold]*len(Rec)
-
-print ('TRecpe NoRec {} '.format(tRecpe(NoRec)))
-print ('TRecpe Rec {} '.format(tRecpe(Rec)))
-# print ('TRecpe T {} '.format(tRecpe(T)))
-
-plt.plot(NoRec, Rec)
-# plt.plot(NoRec, T)
-plt.title('Illumination change')
-plt.NoReclabel('Time,[s]')
-plt.Reclabel('Light level, [LNoRec]')
-plt.grid()
-# plt.NoRecticks(rotation=-90)
-plt.legend(['illumination', 'threshold {} LNoRec'.format(threshold)])
-plt.show()
+create_graph(NoRec, None, RecCash, None, Sec2, 1000)
